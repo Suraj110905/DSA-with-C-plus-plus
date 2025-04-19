5b94f91e3cebeea50d78d3a9e86ae11fd7003759
Suraj Kumar <suraj110905@gmail.com>
2025-04-19 22:32:48 +0530

    type casting

---
+++ first.cpp
@@ -0,0 +1,6 @@
+//program to print hello world
+# include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello world"<<endl;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+int main(){
+
+    int a = 10;
+    char c='b';// ASCII value of 'b' is 98
+    // char c = 98; // this is also valid
+    char e='d';
+    a=c;
+    cout<<a;
+    cout<<
+}
