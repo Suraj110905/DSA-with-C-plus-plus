29eb567a715c4c43220682e092777c58f03bd943
Suraj Kumar <suraj110905@gmail.com>
2025-04-24 21:08:05 +0530

    if else program series

--- ifelseprogram.cpp
+++ ifelseprogram.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int age;
-    cout<<"enter the age to be checked";
+    cout<<"enter the age to be checked";//program to check if a person is eligible to vote or not
     cin>>age;
     if (age>18){
         cout<<"eligible to vote";
