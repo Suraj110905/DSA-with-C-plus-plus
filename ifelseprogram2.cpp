873008092ad9578b847f8a8fecac8b3c199652bd
Suraj Kumar <suraj110905@gmail.com>
2025-04-24 21:15:09 +0530

    if else series program

--- ifelseprogram2.cpp
+++ ifelseprogram2.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main(){
     int num;
-    cout<<"Enter the number to be checked";
+    cout<<"Enter the number to be checked";//program to check number is even or odd
     cin>>num;
     if(num%2==0){
+        //or use logic if(num%2==1)-->it is odd no
         cout<<"Number is even";
     }else{
         cout<<"Number is odd";
