1ccd30451e2806ea035030b7e2159a594166924b
Suraj Kumar <suraj110905@gmail.com>
2025-04-25 23:21:59 +0530

    IF else series programs

--- ifelseprogram3.cpp
+++ ifelseprogram3.cpp
@@ -2,4 +2,8 @@
 using namespace std;
 int main(){
     //program to check if number is positive(+ve) , zero(0) else negative(-ve)
+    int num;
+    cout<<"enter the number to be checked";
+    cin>>num;
+    if()
 }
