5b92accbe345f1b013e4540a434e813df4262401
Suraj Kumar <suraj110905@gmail.com>
2025-05-04 23:49:32 +0530

    ifelse series programming

--- ifelseprogram4.cpp
+++ ifelseprogram4.cpp
@@ -1,5 +1,19 @@
 # include <iostream>
 using namespace std;
 int main(){
-    int 
+    int c = 'a';
+    if(c=='a'){
+        cout<<"vowel";
+
+    else if(c=='e')
+        cout<<"vowel";
+    else if(c=='i')
+        cout<<"vowel";
+    else if(c=='o')
+        cout<<"vowel";
+    else if(c=='u')
+        cout<<"vowel";
+    else
+        cout<<"consonant";
+    }
 }
