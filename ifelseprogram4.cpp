dc6f0cc9155ac17038bf93aa849a973a14881263
Suraj Kumar <suraj110905@gmail.com>
2025-05-06 18:44:00 +0530

    vowel consonant code end

--- ifelseprogram4.cpp
+++ ifelseprogram4.cpp
@@ -1,5 +1,27 @@
 # include <iostream>
 using namespace std;
-int main(){
-    cout<<"enter the alphabet to be checked"
+int main()
+{
+    char c;
+    cout<<"enter the alphabet to be checked";
+    cin>>c;
+
+    if(c =='a')
+    cout<<"vowel";
+
+    else if(c =='e')
+    cout<<"vowel";
+
+    else if(c =='i')
+    cout<<"vowel";
+
+    else if(c =='o')
+    cout<<"vowel";
+
+    else if(c=='u')
+    cout<<"vowel";
+
+    else
+    cout<<"consonant";
+
 }
