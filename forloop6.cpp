674a417e5df6f6e496bf8301a3bb14d9501be3fc
Suraj Kumar <suraj110905@gmail.com>
2025-05-08 23:25:40 +0530

    for loop series

---
+++ forloop6.cpp
@@ -0,0 +1,10 @@
+//write a program to print alphabets from a to z using for loop
+#include <iostream>
+using namespace std;
+int main(){
+    char name;
+    for(name='a';name<='z';name=name+1)//remember to put alphabets into braces 
+    {
+        cout<<name<<endl;
+    }
+}
