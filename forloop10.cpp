122d8cdd904d9d2a89a41a4035a91ce622aeef52
Suraj Kumar <suraj110905@gmail.com>
2025-05-11 22:40:03 +0530

    for loop series

--- forloop10.cpp
+++ forloop10.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
     int n;
     cout<<"enter the number";
-    
-    
+    cin>>n;
+    for(int i=1;i<=10;i=i+1){
+        cout<<n<<"*"<<i<<"="<<n*i<<endl;
+    }
 }
---
+++ forloop11.cpp
@@ -0,0 +1,13 @@
+//write a program to take input from user of number and it power and display its result using for loop
+#include<iostream>
+using namespace std;
+int main(){
+    int n,power;
+    cout<<"enter the number";
+    cin>>n;
+    cout<<"enter the power";
+    cin>>power;
+    for(int i=1;i<power;i=i+1){
+        cout<<
+    }
+}
