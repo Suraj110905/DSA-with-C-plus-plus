6255a5cf6486e3544072b398547a69f0522849ed
Suraj Kumar <suraj110905@gmail.com>
2025-05-22 22:45:55 +0530

    advance for loop series

--- forloop14.cpp
+++ forloop14.cpp
@@ -2,19 +2,18 @@
 # include <iostream>
 using namespace std;
 int main(){
-    int n,t1,t2,nt;
+    int n,t1,t2,t3;
     cout<<"enter the number";
     cin>>n;
     t1=0; 
     
     t2=1;
 
-    nt=t1+t2;
-
-    t1=t2;
-
-    t2=nt;
-    
+    cout<<t1<<endl;
+    cout<<t2<<endl;
+    for(int i=0;i>1;i=i+t3)
+        t3=t1+t2;
+        t1=t2;
+        t2=t3;
+        cout<<t3;
     }
-
-}
