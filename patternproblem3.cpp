62a63bac8cb8e191cab33b1e33aa4fc5af627c50
Suraj Kumar <suraj110905@gmail.com>
2025-07-27 01:17:04 +0530

    pattern problem 3

--- patternproblem3.cpp
+++ patternproblem3.cpp
@@ -1,17 +1,16 @@
-//10 10 10 10 10
-//10 10 10 10 10
-//10 10 10 10 10
-//10 10 10 10 10
-//10 10 10 10 10
+//1 1 1 1 1
+//2 2 2 2 2
+//3 3 3 3 3
+//4 4 4 4 4
+//5 5 5 5 5
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     for(int row=1;row<=5;row++)
     {
         for(int col=1;col<=5;col++)
         {
-            cout<<"10"<<" ";
+            cout<<row<<" ";
         }
         cout<<endl;
     }
