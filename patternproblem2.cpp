a1468b83d55928254bf3ce6d50d13bf9460fceca
Suraj Kumar <suraj110905@gmail.com>
2025-07-27 01:05:20 +0530

    pattern problem 2

--- patternproblem2.cpp
+++ patternproblem2.cpp
@@ -7,5 +7,12 @@
 using namespace std;
 int main()
 {
-    for()
+    for(int row=1;row<=4;row++)
+    {
+        for(int col=1;row<=5;row++)
+        {
+            cout<<"10"<<" ";
+        }
+        cout<<endl;
+    }
 }
