d7c65250bfeea95a1e55dcd6f599d93179b7c8e9
Suraj Kumar <suraj110905@gmail.com>
2025-08-04 23:48:18 +0530

    hard pattern problems

---
+++ patternproblem9.cpp
@@ -0,0 +1,25 @@
+//         1
+//       2 1
+//     3 2 1
+//   4 3 2 1
+// 5 4 3 2 1
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+        for(int i=1;i<=n-i;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                cout<<" ";
+            }
+            for(int j=1;j<=i;j++)
+            {
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+
+}
