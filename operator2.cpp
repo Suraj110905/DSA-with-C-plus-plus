66c8aa6240abbf21b07429e6458f91a1bca2e4db
Suraj Kumar <suraj110905@gmail.com>
2025-08-06 23:55:03 +0530

    relational operator

---
+++ operator2.cpp
@@ -0,0 +1,19 @@
+//2) Relational Operators : are used for the comparison of the values of two operands. For example, '>' check right operand is greater.
+/*
+Name	                Symbol	Description
+Is Equal To	              ==	Checks both operands are equal
+Greater Than	          >	    Checks first operand is greater than the second operand
+Greater Than or Equal To  >=	Checks first operand is greater than equal to the second operand
+Less Than	              <	    Checks first operand is lesser than the second operand
+Less Than or Equal To	  <=	Checks first operand is lesser than equal to the second operand
+Not Equal To	          !=	Checks both operands are not equal
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 6, b = 4;
+
+    //Equal operator
+    
+}
