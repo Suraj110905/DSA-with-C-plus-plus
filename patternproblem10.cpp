d16a4ff9f96c9b2b131b775ddcb872a1d270310e
Suraj Kumar <suraj110905@gmail.com>
2025-08-06 22:40:22 +0530

    hard pattern problems

--- patternproblem10.cpp
+++ patternproblem10.cpp
@@ -22,4 +22,16 @@ int main(){
         }
         cout<<endl;
     }
+for(int row=n;row>=1;row--)
+    {
+        for(int col=1;col<=5-row;col++)
+        {
+            cout<<" ";
+        }
+        for(int col=1;col<=row;col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
 }
