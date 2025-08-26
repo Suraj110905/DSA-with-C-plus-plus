51ede24066f95fd681925ac8e1c13f70130618c5
Suraj Kumar <suraj110905@gmail.com>
2025-08-26 23:59:54 +0530

    updates

--- for1.cpp
+++ for1.cpp
@@ -9,14 +9,14 @@
       //  cout<<"hello"<<endl;
    // }
 //}
-
+// wap to take input from user and print numbers of inputted value
 #include <iostream>
 using namespace std;
 int main(){
     int n,a;
     cout<<"enter numbers";
     cin>>n;
-    for ( a=10;a<=n;a++){
+    for ( a=1;a<=n;a++){
     cout<<a<<endl;
     }
 }
