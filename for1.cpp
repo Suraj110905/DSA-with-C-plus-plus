38df3b70be1c0ede20c95098f0081c8e7a28287f
Suraj Kumar <suraj110905@gmail.com>
2025-08-26 23:55:54 +0530

    updates

--- for1.cpp
+++ for1.cpp
@@ -1,3 +1,4 @@
+//wap to enter a number from the user and print hello n times
 //#include <iostream>
 //using namespace std;
 //int main(){
@@ -15,7 +16,7 @@ int main(){
     int n,a;
     cout<<"enter numbers";
     cin>>n;
-    for ( a=10;a<=n;a-+){
+    for ( a=10;a<=n;a++){
     cout<<a<<endl;
     }
 }
