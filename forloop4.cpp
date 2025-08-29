138762ac1d2f6d2a9bdb368c5de6191213a91756
Suraj Kumar <suraj110905@gmail.com>
2025-08-29 22:34:16 +0530

    approach 2 for even number upto n

--- forloop4.cpp
+++ forloop4.cpp
@@ -14,5 +14,12 @@ int main(){
 # include <iostream>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<endl;
+        }
+    }
 }
