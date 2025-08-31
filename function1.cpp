de4f3140919879088ba96455e38a206acb2fa726
Suraj Kumar <suraj110905@gmail.com>
2025-09-01 01:02:18 +0530

    add two number using user defined functions

--- function1.cpp
+++ function1.cpp
@@ -1,5 +1,17 @@
+//wap to add two numers using user define function
 # include <iostream>
 using namespace std;
+void add(int,int);
 int main(){
-    
+    int a,b;
+    cout<<"enter the number";
+    cin>>a>>b;
+    add(a,b);
+    return 0;
+}
+void add(int a,int b)
+{
+    int c;
+    c=a+b;
+    cout<<"\nAddition="<<c;
 }
