31fb05c0ec4c5e32078f06929f8775f6d7a00282
Suraj Kumar <suraj110905@gmail.com>
2025-08-31 23:25:12 +0530

    facorial using while loop

--- factorial.cpp
+++ factorial.cpp
@@ -15,3 +15,23 @@ int main(){
     }
     return 0;
 }
+
+//approach 2 using while loop
+int main(){
+    int n,fact;
+    cout<<"enter the number";
+    cin>>n;
+    if(n==0)
+        cout<<"\nfactorial is 1";
+    else if(n<0)
+        cout<<"\nfactorial is not possible";
+    else{
+        fact=1
+        while(n>=1)
+        {
+            fact=fact*n;
+            n=n-1;
+        }
+        cout<<"\nfactorial is"<<fact;
+    }
+}
