3e4f2d570f200d6a6a3a4215e88bd327432b97d5
Suraj Kumar <suraj110905@gmail.com>
2025-09-05 23:26:17 +0530

    problems of array

---
+++ array3.cpp
@@ -0,0 +1,21 @@
+//maximum element of an array
+#include <iostream>
+#include <climits>  // for INT_MIN
+using namespace std;
+
+int main() {
+    int arr[] = {2, 3, 11, 8, 7, -2};   // array declaration
+    int n = sizeof(arr) / sizeof(arr[0]);  // number of elements
+
+    int largest = INT_MIN;  // initialize with smallest integer
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];  // update if bigger element found
+        }
+    }
+
+    cout << "Maximum element = " << largest << endl;
+
+    return 0;
+}
