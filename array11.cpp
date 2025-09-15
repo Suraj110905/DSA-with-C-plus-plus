f9678e44dcb08d4fcfd6268e69ecc778fa673731
Suraj Kumar <suraj110905@gmail.com>
2025-09-15 23:51:50 +0530

    array

---
+++ array11.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // store last element
+    int last = arr[n - 1];
+
+    // shift elements to the right
+    for(int i = n - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // put last element in front
+    arr[0] = last;
+
+    // print rotated array
+    cout << "Rotated array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
