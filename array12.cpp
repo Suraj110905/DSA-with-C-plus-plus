471c98cbf727ebb45125bf7fd46a1afe7cf71bcd
Suraj Kumar <suraj110905@gmail.com>
2025-09-23 23:49:22 +0530

    array

--- array12.cpp
+++ array12.cpp
@@ -1,2 +1,23 @@
-//
+//sorting using selection sort
 #include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"enter the size of array";
+    cin>>n;
+    cout<<"enter the element in array";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    for(int i=0;i<n-1;i++)
+    {
+        int index = i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[index])
+            index=j;
+        }
+        swap(arr[index],arr[i]);
+    }
+}
