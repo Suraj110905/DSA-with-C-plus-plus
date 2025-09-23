cf8782085d4a459dc0dd72230921fc14469d2dbf
Suraj Kumar <suraj110905@gmail.com>
2025-09-23 23:52:58 +0530

    array

--- array12.cpp
+++ array12.cpp
@@ -18,6 +18,10 @@ int main()
             if(arr[j]<arr[index])
             index=j;
         }
-        swap(arr[index],arr[i]);
+        swap(arr[i],arr[index]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
     }
 }
