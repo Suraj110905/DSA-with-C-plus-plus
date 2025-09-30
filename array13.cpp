49471ec8fb03b4b1dc7e558aab710e6e416b7210
Suraj Kumar <suraj110905@gmail.com>
2025-09-30 17:04:30 +0530

    array

--- array13.cpp
+++ array13.cpp
@@ -1,2 +1,11 @@
 //Bubble sort with array
 #include <iostream>
+using namespace std;
+int main(){
+    arr[] = {7,4,8,1,5 }
+    for (int j=0;j<4;j++)
+    {
+        if(arr[j]>arr[j+1])
+        swap(arr[j],arr[j+1])
+    }
+}
