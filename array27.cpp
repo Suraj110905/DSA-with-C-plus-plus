639fe8970956c66a6e42ad38abdb09581354d68d
Suraj Kumar <suraj110905@gmail.com>
2025-10-08 21:02:47 +0530

    array

--- array27.cpp
+++ array27.cpp
@@ -1,17 +1,17 @@
-//Find Pairs Whose Sum Equals a Given Number#include <iostream>
+//Find Average of Array Elements
+#include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {1, 4, 5, 6, 3, 2};
-    int n = 6, sum = 7;
+    int arr[] = {10, 20, 30, 40};
+    int n = 4;
+    int sum = 0;
 
-    cout << "Pairs with sum " << sum << ": ";
     for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(arr[i] + arr[j] == sum)
-                cout << "(" << arr[i] << "," << arr[j] << ") ";
-        }
+        sum += arr[i];
     }
-    cout << endl;
+
+    float average = (float)sum / n;
+    cout << "Average of elements: " << average << endl;
     return 0;
 }
